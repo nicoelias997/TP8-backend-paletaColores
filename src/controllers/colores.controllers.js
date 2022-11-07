fa20e90238b5d6cb7dd671c986e74bf51d2afd8b
nicoelias997 <nicoelias997@gmail.com>
2022-11-07 19:58:35 +0100

    agrego y pruebo en thunderclient las peticiones, funcionan correctamente las 5 peticiones solicitadas

--- src/controllers/colores.controllers.js
+++ src/controllers/colores.controllers.js
@@ -1,9 +1,72 @@
+import { validationResult } from "express-validator"
+import { Color } from "../models/color"
 
+export const listarColores = async (req,res) => {
+    try{
+        const colores = await Color.find()
+        res.status(200).json(colores)
 
-export const listarColores = (req,res) => {
-    res.send("Desde el get")
+    } catch (e){
+        res.stauts(404).json({
+            message: "No pudimos encontrar colores."
+        })
+    }
 }
 
-export const crearColor = (req,res) => {
-    res.send("Desde el post")
+export const crearColor = async (req,res) => {
+        try{
+            const errors = validationResult(req)
+            if(!errors.isEmpty()){
+                return res.status(400).json({
+                    message: "Hay un error, no pudimos crear el color."
+                })
+            }
+        const nuevoColor = new Color(req.body)
+        await nuevoColor.save()
+        res.status(201).json({
+            message: "Hemos creado el color con exito."
+        })
+
+    } catch(e){
+        res.stauts(404).json({
+            message: "No pudimos crear el color."
+        })
+    }
+}
+
+export const obtenerColor = async (req, res) => {
+    try{
+        const colorBuscado = await Color.findById(req.params.id)
+        res.status(200).json(colorBuscado)
+    } catch(e){
+        res.status(400).json({
+            message: "No pudimos encontrar el color"
+        })
+    }
+}
+
+export const eliminarColor = async (req, res) => {
+    try{
+        await Color.findByIdAndDelete(req.params.id)
+        res.status(200).json({
+            message: "Eliminamos el color con exito."
+        })
+    } catch(e){
+        res.status(400).json({
+            message: "No pudimos encontrar el color"
+        })
+    }
+}
+
+export const editarColor = async (req, res) => {
+    try{
+        await Color.findByIdAndUpdate(req.params.id, req.body)
+        res.status(200).json({
+            message: "Hemos editado el color con exito"
+        })
+    } catch(e){
+        res.status(400).json({
+            message: "No pudimos encontrar el color"
+        })
+    }
 }
--- src/routes/colores.routes.js
+++ src/routes/colores.routes.js
@@ -1,8 +1,49 @@
-import {Router} from "express"
-import { listarColores, crearColor } from "../controllers/colores.controllers"
+import { Router } from "express";
+import { check } from "express-validator";
+import { listarColores, crearColor, obtenerColor, eliminarColor, editarColor } from "../controllers/colores.controllers";
 
-const router = Router()
+const router = Router();
 
-router.route("/colores").get(listarColores).post(crearColor)
+router
+  .route("/colores")
+  .get(listarColores)
+  .post(
+    [
+      check("nombreColor", "El nombre del color debe ser obligatorio")
+        .notEmpty()
+        .withMessage("El color no puede ser un campo vacio.")
+        .trim()
+        .withMessage("El color no puede ser un campo vacio.")
+        .isIn([
+          "red",
+          "pink",
+          "purple",
+          "deepPurple",
+          "indigo",
+          "blue",
+          "lightBlue",
+          "cyan",
+          "teal",
+          "green",
+          "lightGreen",
+          "lime",
+          "yellow",
+          "amber",
+          "orange",
+          "deepOrange",
+          "brown",
+          "grey",
+          "blueGrey",
+        ])
+        .withMessage("El color debe ser escrito en camelCase y en ingles!"),
+    ],
+    crearColor
+  );
 
-export default router
+  router
+  .route("/colores/:id")
+  .get(obtenerColor)
+  .delete(eliminarColor)
+  .put(editarColor)
+
+export default router;
