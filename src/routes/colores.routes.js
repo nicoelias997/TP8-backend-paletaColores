1e517f6695f400ab1e8c95b690a43ddd92b0917a
nicoelias997 <nicoelias997@gmail.com>
2022-11-07 19:59:43 +0100

    Agrego la  validacion  ala peticion de editar color

--- src/routes/colores.routes.js
+++ src/routes/colores.routes.js
@@ -44,6 +44,36 @@ router
   .route("/colores/:id")
   .get(obtenerColor)
   .delete(eliminarColor)
-  .put(editarColor)
-
+  .put(
+    [
+      check("nombreColor", "El nombre del color debe ser obligatorio")
+        .notEmpty()
+        .withMessage("El color no puede ser un campo vacio.")
+        .trim()
+        .withMessage("El color no puede ser un campo vacio.")
+        .isIn([
+          "red",
+          "pink",
+          "purple",
+          "deepPurple",
+          "indigo",
+          "blue",
+          "lightBlue",
+          "cyan",
+          "teal",
+          "green",
+          "lightGreen",
+          "lime",
+          "yellow",
+          "amber",
+          "orange",
+          "deepOrange",
+          "brown",
+          "grey",
+          "blueGrey",
+        ])
+        .withMessage("El color debe ser escrito en camelCase y en ingles!"),
+    ],
+    editarColor
+  );
 export default router;
